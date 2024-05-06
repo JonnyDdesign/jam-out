103bf8d983d595f88baf1d0e5a00587e834aca6f
JonnyDdesign <jondesserres@gmail.com>
2024-05-06 15:33:05 -0700

    adjusted comment Lines

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,10 +8,10 @@ class App extends React.Component {
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
-        <!-- Add a SearchBar component -->
+        // Add a SearchBar component
         <div className="App-playlist">
-          <!-- Add a SearchResults component -->
-          <!-- Add a Playlist component -->
+          // Add a SearchResults component
+          // Add a Playlist component
         </div>
       </div>
     </div>
