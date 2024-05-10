9ace3cd676722f486c379a36b9b0d577804b06f2
JonnyDdesign <jondesserres@gmail.com>
2024-05-10 11:52:51 -0700

    Used the .map() method to render each track in the tracks property.

--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -6,7 +6,9 @@ export class Tracklist extends React.Component {
     render() {
         return (
             <div className="TrackList">
-                // You will add a map method that renders a set of Track components
+                {this.props.tracks.map((song) => {
+                    return <Track key={song.id} track={song} />;
+                })}    
             </div>
         );
     }
