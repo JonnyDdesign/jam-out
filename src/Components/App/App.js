bc161b38b680708f40169ce9d3c0041e6db95d4b
JonnyDdesign <jondesserres@gmail.com>
2024-05-14 22:57:32 -0700

    Bind the current value of this to .addTrack(). Passed .addTrack() to the SearchResults component as an onAdd attribute.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -38,6 +38,7 @@ class App extends React.Component {
         },
       ],
     };
+    this.addTrack = this.addTrack.bind(this);
   }
 
   addTrack(track) {
@@ -55,7 +56,7 @@ class App extends React.Component {
       <div className="App">
         <SearchBar />
         <div className="App-playlist">
-          <SearchResults searchResults={this.state.searchResults} />
+          <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
           <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
         </div>
       </div>
