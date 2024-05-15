bedf08d23dc1e8fa694b76fbb04a48643139dfe0
JonnyDdesign <jondesserres@gmail.com>
2024-05-14 21:46:20 -0700

    Imported Tracklist into Playlist.js. Passed the playlist tracks from the Playlist component to the TrackList component.

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Tracklist } from '../Tracklist/Tracklist';
 import './Playlist.css';
 
 export class Playlist extends React.Component {
@@ -6,7 +7,7 @@ export class Playlist extends React.Component {
         return (
             <div className="Playlist">
                 <input defaultValue="New Playlist"/>
-                // Add a TrackList component
+                <Tracklist tracks={this.props.playlistTracks} />
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         );
