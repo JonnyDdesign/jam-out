8b8eb500dd3e37758a30573f2a3b213d2556ac65
JonnyDdesign <jondesserres@gmail.com>
2024-05-21 10:15:16 -0700

    Created a method named handleTermChange that accepts an event argument and sets the state of the search bar's term to the event target's value. Bound the current value of this to this.handleTermChange to the constructor method. Added an onChange attribute set equal to this.handleTermChange to the search bar's input element.

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -6,16 +6,24 @@ export class SearchBar extends React.Component {
         super(props);
         this.state({ term: "" });
         this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
     }
 
     search() {
         this.props.onSearch(this.state.term);
     }
 
+    handleTermChange(e) {
+        this.setState({ term: e.target.value });
+    }
+
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
+                <input
+                    placeholder="Enter A Song, Album, or Artist"
+                    onChange={this.handleTermChange}
+                />
                 <button className="SearchButton">SEARCH</button>
             </div>
         );
