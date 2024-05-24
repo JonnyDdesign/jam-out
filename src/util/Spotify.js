b6a6cfd230f6ef7db598de0c056507c4402f66ca
JonnyDdesign <jondesserres@gmail.com>
2024-05-23 20:44:39 -0700

    Created a method called getAccessToken to chek if the user's access token is already set, and if it is, return the value saved to access token. If the access token is not already set, check the URL to see if it has just been obtained. Used window.location.href and the .match() method to retrieve access token and expiration time from the URL.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,12 @@
 const accessToken = "";
-const Spotify = {};
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) {
+            return accessToken;
+        }
+        window.location.href.match(/access_token=([^&]*)/);
+        window.location.href.match(/expires_in=([^&]*)/)
+    }
+};
 
 export { Spotify };
