5da3cde1933d14943253f5bf0a0de7d501706a82
JonnyDdesign <jondesserres@gmail.com>
2024-05-23 20:48:54 -0700

    Created a variable in Spotify.js for the clientID. and also a variable for redirectURI.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,6 @@
-const accessToken = "";
+let accessToken = "";
+const clientID = "b996a6f370c14e4c923c4adb16de1f3a";
+const redirectURI = "http://localhost:8080/";
 const Spotify = {
     getAccessToken() {
         if (accessToken) {
