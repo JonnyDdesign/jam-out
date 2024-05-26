a52e0e72d0b6b867d6c481d358e92396562130bc
JonnyDdesign <jondesserres@gmail.com>
2024-05-25 22:10:35 -0700

    Created a method called search that accepts a term parameter. Inside .search(), started the promise chain by returning a GET request, using fetch(), to a Spotify endpoint. Also, added an Authorization header to the request containing the access token.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -17,7 +17,14 @@ const Spotify = {
             const redirect = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`
             window.location = redirect;
         }
-    }
+    },
+
+    search(term) {
+        const accessToken = Spotify.getAccessToken()
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+            headers: { Authorization: `Bearer ${accessToken}` },
+        });        
+    },
 };
 
 export { Spotify };
