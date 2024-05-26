3258525f764505660d7f003baabd1db7963a1b5d
JonnyDdesign <jondesserres@gmail.com>
2024-05-25 21:49:20 -0700

    In the conditional statement, redirected users to the listed URL and interpolated the client ID and redirect URI vaiables.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,6 @@
 let accessToken = "";
 const clientID = "b996a6f370c14e4c923c4adb16de1f3a";
-const redirectURI = "http://localhost:8080/";
+const redirectURI = "http://localhost:3000/";
 const Spotify = {
     getAccessToken() {
         if (accessToken) {
@@ -13,6 +13,9 @@ const Spotify = {
             const expiresIn = Number(urlExpiresIn[1]);
             window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
             window.history.pushState("Access Token", null, "/");
+        } else {
+            const redirect = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`
+            window.location = redirect;
         }
     }
 };
